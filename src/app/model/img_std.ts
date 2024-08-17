b27b125e11220ca3fc5fa204249192e647d89b6e
teerapongtha <teerapong.tha@ku.th>
2024-08-18 04:12:04 +0700

    Remove to new
    ลบไฟล์ทั้งหมดออกจาก repository

--- src/app/model/img_std.ts
+++
@@ -1,5 +0,0 @@
-export interface img_std {
-    img_id: number;
-    img_path: string;
-    std_id : string;
-}
--- src/app/model/student.model.ts
+++
@@ -1,9 +0,0 @@
-export interface Student {
-    std_id: string;
-    email: string;
-    password: string;
-    title: string;
-    fname: string;
-    lname: string;
-    img_profiles: string;
-}
